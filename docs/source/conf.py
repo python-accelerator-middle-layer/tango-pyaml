a28838883ecf653f91395474814c80000b534b91
Teresia Olsson <teresia.olsson@helmholtz-berlin.de>
2026-09-09 16:45:28 +0200

    Add docs.

---
+++ docs/source/conf.py
@@ -0,0 +1,77 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "tango-pyaml"
+copyright = "2026, pyAML Collaboration"
+author = "pyAML Collaboration"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "myst_parser",
+    "sphinx_copybutton",
+]
+
+autosummary_generate = True
+
+# Maybe add "undoc-members": True here later
+autodoc_default_options = {
+    "members": True,
+    "show-inheritance": True,
+    "member-order": "groupwise",
+}
+
+autodoc_typehints = "description"
+autodoc_typehints_description_target = "documented"
+autodoc_typehints_format = "short"
+# autosummary_generate_overwrite = False
+# autosummary_ignore_module_all = False
+autoclass_content = "both"  # include both class docstring and __init__
+
+napoleon_use_rtype = False  # More legible
+# napoleon_numpy_docstring = False  # Force consistency, leave only Google
+# napoleon_custom_sections = [("Returns", "params_style")]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+html_title = "tango-pyaml"
+html_show_sourcelink = False
+html_css_files = ["custom.css"]
+html_logo = "_static/_images/logo.png"
+
+html_theme_options = {
+    "navigation_depth": 4,
+    "show_nav_level": 2,
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/python-accelerator-middle-layer/tango-pyaml",
+            "icon": "fa-brands fa-github",
+            "type": "fontawesome",
+        },
+    ],
+}
+
+html_sidebars = {
+    "**": [
+        "sidebar-collapse",
+        "sidebar-nav-bs",
+    ],
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,16 @@
+
+.. include:: ../../README.md
+   :parser: myst_parser.sphinx_
+
+.. seealso::
+
+   This package is part of the
+   `pyAML ecosystem <https://python-accelerator-middle-layer.github.io/>`__.
+   See the `main pyAML documentation
+   <https://python-accelerator-middle-layer.github.io/documentation/>`__
+   for tutorials, how-to guides, and an overview of the ecosystem.
+
+.. toctree::
+   :hidden:
+
+   api
